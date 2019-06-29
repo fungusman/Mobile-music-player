447a3030df49636870ea2964354ae311ad8cf935
fungusman <fuanfun22@163.com>
2019-06-29 10:33:20 +0800

    定义主题，完成header部分

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import "./style.scss";
-
-const title = document.createElement("h1");
-title.innerHTML = "Let start 音乐播放器";
-document.body.appendChild(title);
-console.error("报点")
+import "./scss/index.scss";
